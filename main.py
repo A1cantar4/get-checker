36e77e27eb3a8a2f61cc7697738339637a6659ce
Lucas <alcantaralucas2002@gmail.com>
2025-08-26 12:51:22 -0300

    Moved to root

---
+++ main.py
@@ -0,0 +1,15 @@
+from get_checker.checker import check_website
+from get_checker.logger import setup_logger
+import time
+
+def main():
+    logger = setup_logger()
+    url = input("Paste here the link: ").strip()
+    interval = 6
+
+    while True: # Press 'CTRL + C' to break the loop
+        check_website(url, logger)
+        time.sleep(interval)
+        
+if __name__ == "__main__":
+    main()
